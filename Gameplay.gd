23d6ec01ba84d4460d492e71e565ad54b8aff375
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-01-31 20:12:06 -0500

    feat: updated cards system

--- Gameplay.gd
+++ Gameplay.gd
@@ -6,11 +6,10 @@ const HAND_WIDTH = 10.0
 @export var height_curve: Curve
 @export var rotation_curve: Curve
 
-@onready var selectedCard = bool 
+var aCardSelected = false
 
 func _ready():
 	var deck_node = $deck 
-	tween = get_tree().create_tween()
 	for _x in 5:
 		var card = CARD.instantiate()
 		card.name = "Card_" + str(_x)
@@ -34,15 +33,37 @@ func _position_cards():
 		destination.y -= height_curve.sample(deck_node_ratio) * 100.0
 		card.rotation = rotation_curve.sample(deck_node_ratio) * 0.2
 		card.position = destination
+		card.get_child(1).text = str(card.get_index())
 
-func _on_card_pressed(card):
-	print("Hello, World!")
+func _position_rest(selectedCard):
 	var deck_node = $deck 
+	for card in deck_node.get_children():
+		if card == selectedCard:
+			break
+		var deck_node_ratio = 0.5
 
-	# Get the pressed card
-	var pressed_card = get_node("deck/" + card.name)
-	var animation_player = pressed_card.get_node("AnimationPlayer")
-	animation_player.play("card_move_up")
+		if deck_node.get_child_count() > 1:
+			deck_node_ratio = float(card.get_index()) / float(deck_node.get_child_count() - 2)
 
+		var destination = deck_node.position
+		destination.x += spread_curve.sample(deck_node_ratio) * -400.0
+		destination.y -= height_curve.sample(deck_node_ratio) * 100.0
+		card.rotation = rotation_curve.sample(deck_node_ratio) * 0.2
+		card.position = destination
+		
+func _on_card_pressed(card):
+	var deck_node = $deck 
+	print(aCardSelected)
+	if not aCardSelected:
+		aCardSelected = true
+		var pressed_card = get_node("deck/" + card.name)
+		pressed_card.rotation = 0
+		var animation_player = pressed_card.get_node("AnimationPlayer")
+		animation_player.play("card_move_up")
+		_position_rest(pressed_card)
+	else:
+		aCardSelected = false
+		_position_cards()
+		
 func _process(delta):
 	pass
